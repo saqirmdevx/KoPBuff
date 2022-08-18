d69f065158769fb0947b9d88dbe44bd4f3fead74
anon <>
2022-08-18 09:58:19 -0400

    cleanup partical_match.vue

--- src/components/partial_match.vue
+++ src/components/partial_match.vue
@@ -1,9 +1,7 @@
 <template>
        <!-- `@/assets/creatures/heroes/${heros_reversed[hero]}/hero_face.png`)} -->
    <div ref="partialMatch" class="partial-match mb-2" @click="onClick(player.game_id)">
-        <img class="hero-img" 
-        :src="require(`../assets/creatures/heroes/${heros_reversed[player.hero]}/hero_face.png`)"
-        width="32" height="32">
+        <HeroIcon :hero_name="player.hero" width="32" height="32"></HeroIcon>
         <span class="kills">{{player.kills}}</span>
         <span>/</span>
         <span class="assists">{{player.assists}}</span>
@@ -14,42 +12,17 @@
 
 <script>
 
-const heros_reversed = {
-  "Kumihu": 0,
-  "Sparrow": 1,
-  "Belle": 2,
-  "Iceat": 3,
-  "Thomas": 4,
-  "Veil": 5,
-  "Flin": 6,
-  "Kira": 7,
-  "Hazel": 8,
-  "Arel": 9,
-  "Alvar": 10,
-}
-
-
-import item_names_to_png_names from "./../assets/item_names_to_png_names.json";
+import HeroIcon from './hero_icon.vue';
 
 export default {
     name: "KopMatchHistory",
 
-    props:{
-        player: Object
-        // gameId: String,
-
-        // gamemode: String,
-        // winningTeam: String,
-        // gameDuration: String,
-        // gameHerosBlue: Array,
-        // gameHerosRed: Array,
+    components: {
+        HeroIcon,
     },
 
-    data () {
-        return {
-            item_names_to_png_names,
-            heros_reversed
-        }
+    props:{
+        player: Object
     },
 
     methods: {
